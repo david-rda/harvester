849d32e3f170464e07030f6f4df1e1a34fed6417
= <=>
2022-12-13 13:36:54 +0400

    bugs fixed

--- server/app/Http/Interfaces/IRegister.php
+++ server/app/Http/Interfaces/IRegister.php
@@ -2,12 +2,10 @@
     namespace App\Http\Interfaces;
 
     use Illuminate\Http\Request;
-    use App\Http\Requests\AuthRequest;
-    use App\Http\Requests\RegisterRequest;
 
     interface IRegister {          
-        public function ValidateRegisterData(RegisterRequest $request); // მომხმარებლის სარეგისტრაციო მონაცემების გაფილტვრა
+        public function ValidateRegisterData(Request $request); // მომხმარებლის სარეგისტრაციო მონაცემების გაფილტვრა
         
-        public function Register(RegisterRequest $request); // მომხმარებლის რეგისტრაციის მეთოდი
+        public function Register(Request $request); // მომხმარებლის რეგისტრაციის მეთოდი
     }
 ?>
