6f7ab68001da7c8d777c5609ae23c2e68bae9512
= <=>
2022-12-20 12:43:39 +0400

    created file opening functions

--- client/src/views/StatementAdd.vue
+++ client/src/views/StatementAdd.vue
@@ -156,7 +156,7 @@
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
                                     <input type="file" v-on:change="handlePersonalId($event)" ref="personal_id_file">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openPid()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openPersonalIdFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>პოტენციური ბენეფიციარის პირადობის მოწმობის ასლი.</span>
                                 </div>
                             </div>
@@ -166,7 +166,7 @@
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
                                     <input type="file" v-on:change="handlePersonalId1($event)" ref="personal_id_file1">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openPid1()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>წარმომადგენლობაზე უფლებამოსილი პირის პირადობის მოწმობის ასლი და უფლებამოსილების დამადასტურებელი დოკუმენტი (ასეთის არსებობის შემთხვევაში).</span>
                                 </div>
                             </div>
@@ -175,8 +175,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile3($event)" ref="file3">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile3()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile3($event)" ref="entrepreneurFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openEntrepreneurDocument()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>ამონაწერი მეწარმეთა და არასამეწარმეო (არაკომერციული) იურიდიული პირების რეესტრიდან (იურიდიული პირის შემთხვევაში).</span>
                                 </div>
                             </div>
@@ -186,8 +186,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile4($event)" ref="file4">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile4()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile4($event)" ref="notaryFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFileNotary()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>იურიდიული პირების შემთხვევაში − ნოტარიული წესით დამოწმებული დამფუძნებლების/პარტნიორების კრების ოქმი პროგრამაში მონაწილეობისა და სააგენტოსთან გასაფორმებელი ხელშეკრულებით გათვალისწინებული ვალდებულებების აღების თაობაზე (ასეთის საჭიროების შემთხვევაში).</span>
                                 </div>
                             </div>
@@ -197,8 +197,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile5($event)" ref="file5">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile5()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile5($event)" ref="invoiceFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openInvoiceFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>შესასყიდი მიზნობრიობის ინვოისი ან/და ხელშეკრულება ქართულ ენაზე (ტექნიკის იმპორტის შემთხვევაში ნოტარიულად დამოწმებული თარგმანი), რომელიც აუცილებელი წესით უნდა მოიცავდეს შემდეგ ინფორმაციას: მოსავლის ამღები სასოფლო-სამეურნეო ტექნიკის დასახელება, მახასიათებლები (მოდელი, ცხ.ძ (ასეთის არსებობის შემთხვევაში), გამოშვების წელი, ექსპლუატაციაში არ ყოფნის შესახებ სტატუსი, მწარმოებელი ქვეყანა, დანიშნულება (აღწერილი უნდა იყოს თუ რა სასოფლო-სამეურნეო დანიშნულებით გამოიყენება წარმოდგენილი ტექნიკა), მხარეთა რეკვიზიტები, მიწოდების ვადა (არაუმეტეს 5 თვისა), გადახდის პირობები. შენიშვნა: ტექნიკის ადგილობრივი მომწოდებლის შემთხვევაში გადახდის პირობა აუცილებელი წესით უნდა მოიცავდეს შემდეგ ინფორმაციას, რომ სააგენტოს თანადაფინანსების თანხის გადახდა მოხდება ტექნიკის მიწოდების შემდეგ.</span>
                                 </div>
                             </div>
@@ -208,8 +208,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile6($event)" ref="file6">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile6()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile6($event)" ref="grantFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openGrantDocumentFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>სასოფლო-სამეურნეო ტექნიკის საგარანტიო დოკუმენტი (საგარანტიო პირობები უნდა ვრცელდებოდეს განაცხადის წარდგენიდან არანაკლებ 6 თვის ვადით) გაცემული მწარმოებლის ან/და მიმწოდებლის მიერ.</span>
                                 </div>
                             </div>
@@ -219,8 +219,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile7($event)" ref="file7">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile7()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile7($event)" ref="inquiryFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openInquiryFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>ცნობა ან/და შედარების აქტი სსიპ - შემოსავლების სამსახურიდან დავალიანების არარსებობის შესახებ.</span>
                                 </div>
                             </div>
@@ -230,8 +230,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile8($event)" ref="file8">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile8()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile8($event)" ref="complicityFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openComplicityFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>თანამონაწილეობის თანხის დამადასტურებელი დოკუმენტი - ცნობა/ამონაწერი/ხელშეკრულება ბანკიდან შესაბამისი თანხის ანგარიშზე არსებობის შესახებ ან/და ცნობა ბანკიდან / საფინანსო ინსტიტუტიდან კრედიტის/ლიზინგის/ დამტკიცების თაობაზე.</span>
                                 </div>
                             </div>
@@ -241,8 +241,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile9($event)" ref="file9">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile9()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile9($event)" ref="accountFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="bankAccountDetails()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>საბანკო რეკვიზიტები.</span>
                                 </div>
                             </div>
@@ -252,8 +252,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile10($event)" ref="file10">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile10()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile10($event)" ref="examinFile">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openExaminationFile()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>სსიპ ლევან სამხარაულის სახელობის სასამართლო ექსპერტიზის ბიუროს ან/და სსიპ „აკრედიტაციის ეროვნული ორგანოს - აკრედიტაციის ცენტრის“ მიერ აკრედიტირებული პირების ან/და საქართველოს ფინანსთა სამინისტროს სისტემაში შემავალი სახელმწიფო საქვეუწყებო დაწესებულების - ბუღალტრული აღრიცხვის, ანგარიშგებისა და აუდიტის ზედამხედველობის სამსახურის მიერ წარმოებულ რეესტრში არსებული პირის საექსპერტო/აუდიტორული დასკვნა წარმოდგენილი პროექტის საინვესტიციო ღირებულების საბაზრო ფასებთან შესაბამისობის შესახებ.</span>
                                 </div>
                             </div>
@@ -263,8 +263,8 @@
                         <div class="row">
                             <div class="mb-3 mt-4 col-lg-12 col-md-12 col-xs-12 col-sm-12">
                                 <div class="file_block p-3">
-                                    <input type="file" v-on:change="handleFile11($event)" ref="file11">
-                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openFile11()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
+                                    <input type="file" v-on:change="handleFile11($event)" ref="additional">
+                                    <button type="button" class="col-md-3 col-lg-3" v-on:click="openAdditionalFiles()">ფაილის არჩევა&nbsp;<BIconFolder2 /></button>
                                     <span>სხვა დამატებითი დოკუმენტაცია, სააგენტოს მიერ მოთხოვნის შემთხვევაში.</span>
                                 </div>
                             </div>
@@ -315,6 +315,19 @@
     import Header from "../components/Header.vue";
     import Footer from "../components/Footer.vue";
     import axios, { AxiosError } from "axios";
+    import {
+        openPersonalIdFile,
+        openFile,
+        openEntrepreneurDocument,
+        openFileNotary,
+        openInvoiceFile,
+        openGrantDocumentFile,
+        openInquiryFile,
+        openComplicityFile,
+        bankAccountDetails,
+        openExaminationFile,
+        openAdditionalFiles
+    } from "../helpers/openFile"
     
     export default {
         name : "StatementAdd",
@@ -395,21 +408,18 @@
                 }
             },
 
-            openPid() {
-                this.$refs.personal_id_file.click();
-            },
-
-            openPid1() {
-                this.$refs.personal_id_file1.click();
-            },
-
-            openFile3() {
-                this.$refs.file3.click();
-            },
-
-            openFile4() {
-                this.$refs.file4.click();
-            }
+            // ფაილების ატვირთვისთვის ფანჯრის გახსნის მეტოდები
+            openPersonalIdFile,
+            openFile,
+            openEntrepreneurDocument,
+            openFileNotary,
+            openInvoiceFile,
+            openGrantDocumentFile,
+            openInquiryFile,
+            openComplicityFile,
+            bankAccountDetails,
+            openExaminationFile,
+            openAdditionalFiles
         }
     }
 </script>
