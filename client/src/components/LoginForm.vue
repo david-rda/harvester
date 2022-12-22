970d2a76b7ff3584ed6e9f625f1bbb5cbab194e1
= <=>
2022-12-22 14:40:41 +0400

    fixed bugs

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -75,7 +75,7 @@
                             position : "top-end"
                         });
                         return false;
-                    }else if(!this.email.match(/^[a-zA-Z0-9]*\.+[a-zA-Z0-9]+@{1}[a-zA-Z]+\.[a-zA-Z]{2,3}$/)) {
+                    }/*else if(!this.email.match(/^[a-zA-Z0-9]*\.+[a-zA-Z0-9]+@{1}[a-zA-Z]+\.[a-zA-Z]{2,3}$/)) {
                         this.$swal({
                             title : "შეიყვანეთ სწორი ფორმატის ელ. ფოსტა",
                             icon : "warning",
@@ -84,7 +84,7 @@
                             toast : true,
                             position : "top-end"
                         });
-                    }else if(this.password == null || this.password == "") {
+                    }*/else if(this.password == null || this.password == "") {
                         this.$swal({
                             title : "პაროლი აუცილებელია",
                             icon : "warning",
