f103cf169c78d40ae2caab40b5fffe231f563f85
= <=>
2023-01-11 14:57:43 +0400

    added some data on details page

--- client/src/views/Details.vue
+++ client/src/views/Details.vue
@@ -7,11 +7,40 @@
             </div>
 
             <div class="row">
-                <table>
-                    <tbody>
+                <div class="col-md-7 col-lg-7">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <th>განმცხადებლის სახელი</th>
+                                <td>{{ this.statement?.applicant_name }}</td>
+                            </tr>
+                            <tr>
+                                <th>განმცხადებლის გვარი</th>
+                                <td>{{ this.statement?.applicant_lastname }}</td>
+                            </tr>
+                            <tr>
+                                <th>მობილური ტელეფონის ნომერი</th>
+                                <td>{{ this.statement?.applicant_phone }}</td>
+                            </tr>
+                            <tr>
+                                <th>დამატებითი ტელეფონის ნომერი</th>
+                                <td>{{ this.statement?.applicant_additional_phone }}</td>
+                            </tr>
+                            <tr>
+                                <th>ელ. ფოსტა</th>
+                                <td>{{ this.statement?.beneficiary_email }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-                    </tbody>
-                </table>
+                <div class="row">
+                    <h6 class="col-lg-6 col-md-6">ზოგადი ინფორმაცია პოტენციურ ბენეფიციარზე</h6>
+                </div>
+
+                <div class="row">
+                    
+                </div>
             </div>
         </div>
         <Footer />
@@ -42,7 +71,11 @@
             this.$store.dispatch("setRole");
 
             try {
-                const statement_data = await axios.get("/statement/get/" + this.$route.params.id);
+                const statement_data = await axios.get("/statement/get/" + this.$route.params.id, {
+                    headers : {
+                        "Authorization" : `Bearer ${this.$store.state.token}`
+                    }
+                });
                 this.statement = statement_data?.data;
                 console.log(this.statement);
             }catch(err) {
@@ -54,6 +87,9 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    table {
+        font-family: "frutiger_light_caps";
+        font-size: 14px;
+    }
 </style>
